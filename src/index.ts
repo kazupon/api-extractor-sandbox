b83e21c6502628ef5f5cf3a8668696edff8437bf
kazuya kawaguchi <kawakazu80@gmail.com>
2020-06-17 01:48:03 +0900

    updates

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+/**
+ * add function
+ * 
+ * @param a target 1
+ * @param b target 2
+ * 
+ * @public
+ */
+export function add(a: number, b: number): number {
+  return a + b
+}
+
+/**
+ * Widget interface
+ * 
+ * @public
+ */
+export interface IWidget {
+  /**
+   * add method 
+   * @param a target 1
+   * @param b target 2
+   * @public
+   */
+  add(a: number, b: number): number
+}
+
+/**
+ * Widget class
+ * @public
+ */
+export class Widget implements IWidget {
+  constructor () {
+
+  }
+
+  /**
+   * add method 
+   * @param a target 1
+   * @param b target 2
+   * @public
+   */
+  add(a: number, b: number): number {
+    return a + b
+  }
+}
