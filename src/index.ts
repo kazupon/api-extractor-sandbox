2859d14f6b8dfe7302f02e8265f9088b87804463
kazuya kawaguchi <kawakazu80@gmail.com>
2020-06-22 01:11:46 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,18 @@ export function add(a: number, b: number): number {
 }
 
 /**
+ * sub function
+ * 
+ * @param a target 1
+ * @param b target 2
+ * 
+ * @public
+ */
+export function sub(a: number, b: number): number {
+  return a - b
+}
+
+/**
  * Widget interface
  * 
  * @public
